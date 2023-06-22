15bda4d7b213d76ed3f9c9d976c67f47df1569bf
Roxana-Irimia <roxana@axiologic.net>
2023-06-22 14:51:08 +0300

    Update remote enclave launcher script

--- psknode/bin/scripts/enclaveLauncher.js
+++ psknode/bin/scripts/enclaveLauncher.js
@@ -9,21 +9,37 @@ const RemoteEnclave = require('remote-enclave');
 path = require("swarmutils").path;
 const fs = require('fs');
 
-if(!process.env.REMOTE_ENCLAVE_CONFIG_LOCATION) {
+if (!process.env.REMOTE_ENCLAVE_CONFIG_LOCATION) {
     process.env.REMOTE_ENCLAVE_CONFIG_LOCATION = "./";
 }
 
 function startServer() {
-    const configFilePath = path.join(process.env.REMOTE_ENCLAVE_CONFIG_LOCATION, 'remote-enclave.json');
-    let config = undefined;
-    try {
-        config = JSON.parse(fs.readFileSync(configFilePath));
-    } catch (e) {
+    const args = process.argv;
+    let options;
+    let config;
+    if (args.length <= 2) {
         console.log(`[${TAG}] No config file found, RemoteEnclave will start using default config`);
+    }
+    try {
+        let fileContent;
+        try {
+            fileContent = fs.readFileSync(args[2]);
+        }
+        catch (err) {
+            console.log(`[${TAG}] Could not read configuration file: ${args[2]}`);
+        }
+
+        options = JSON.parse(fileContent);
+        process.env.REMOTE_ENCLAVE_DOMAIN = options.domain;
+        process.env.REMOTE_ENCLAVE_SECRET = options.config.secret;
+        config = options.config;
+        console.log(`[${TAG}] Working directory for Remote Enclave process:  ${process.cwd()}`);
+    } catch (err) {
+        console.log(`[${TAG}] Config file invalid, RemoteEnclave will start using default config, err: ${err}`);
     }
 
     const remoteEnclaveServer = RemoteEnclave.createInstance(config);
-    remoteEnclaveServer.on("initialised", (did)=>{
+    remoteEnclaveServer.on("initialised", (did) => {
         console.log(`[${TAG}] RemoteEnclave initialised with DID: ${did}`);
     });
 
