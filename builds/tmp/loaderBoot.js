98548655d45d249cb4ad089270de4c109894601d
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-06-29 17:30:33 +0300

    opendsu bundles cleanup

---
+++ builds/tmp/loaderBoot.js
@@ -0,0 +1,21 @@
+if (typeof(window) !== "undefined") {
+    if (typeof(global) !== "undefined") {
+        global = window;
+    }else
+    {
+        window.global = window;
+    }
+}
+
+if (typeof(global.$$) == "undefined") {
+    global.$$ = {};
+    $$.requireBundle = function () {
+    };
+}
+const or = require('overwrite-require');
+or.enableForEnvironment(or.constants.BROWSER_ENVIRONMENT_TYPE);
+
+if (typeof($$.__runtimeModules) == "undefined") {
+    $$.__runtimeModules = {};
+}
+require("./loaderBoot_intermediar");
