073cf57a31620e253e5f1107f7cef713fbd7bdbd
skutner <sava.dumitru.daniel@gmail.com>
2023-09-12 22:47:28 +0300

    cleanup

--- psknode/bin/scripts/migrations/100_rotateSecretsEncryptionKey.js
+++ psknode/bin/scripts/migrations/100_rotateSecretsEncryptionKey.js
@@ -3,7 +3,6 @@ const rotateKeyAsync = async () => {
     const path = require("path");
     console.log("Attempting to rotate secrets encryption key...");
     const apihubRootFolder = apihubModule.getServerConfig().storage;
-    console.log("apihubRootFolder", apihubRootFolder);
     const secretsService = await apihubModule.getSecretsServiceInstanceAsync(apihubRootFolder);
     await secretsService.rotateKeyAsync();
 }
