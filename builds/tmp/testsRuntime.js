aa892d840c1053224bd8c56fff993e1d4320ebc3
skutner <sava.dumitru.daniel@gmail.com>
2023-11-06 15:57:09 +0200

    axiologic-pla/dev##616 set default verbosity to "debug" for tests

--- builds/tmp/testsRuntime.js
+++ builds/tmp/testsRuntime.js
@@ -1,4 +1,5 @@
 const or = require('overwrite-require');
 or.enableForEnvironment(or.constants.NODEJS_ENVIRONMENT_TYPE);
+$$.debug.verbosity("debug");
 require("./testsRuntime_intermediar");
 require("double-check");
